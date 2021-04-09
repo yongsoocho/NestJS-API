c98f3a9b9c235854d4a5336c3e0a805df8ac1f02
yongsoocho <yongsoocho578@gmail.com>
2021-04-09 14:55:48 +0000

    2021/04/09/1.0.0

---
+++ hi-nest/src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [],
+  controllers: [],	// take urls and execute function, like express router(urls, function)
+  providers: [],
+})
+
+export class AppModule {}
